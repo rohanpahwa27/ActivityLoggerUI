7ec73d46c6db9f7eb52e75b67809f35677e183b5
Eric Patel <ericpatelofficial@gmail.com>
2020-10-15 10:17:29 -0400

    call now sends signup payload

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
+from flask import Flask, request, render_template
+import reqs
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("pages/index.html")
 
-@app.route("signup")
+@app.route("/signup")
 def signupPage():
-    return render_template("pages/signup")
+    return render_template("pages/signup.html")
 
-@app.route("signup")
+@app.route("/signup", methods=["POST"])
 def signup():
-    response = reqs.signup(request.form["email"], request.form["first"], request.form["last"], reequest.form["password"])
-    if response.code != "OK":
-        return render_template("pages/signup", error=response.error)
+    print(request.form)
+    if request.form["password"] != request.form["confirm_password"]:
+        return render_template("pages/signup.html", error="Passwords do not match")
+
+    response = reqs.signup(request.form["email"], request.form["first_name"], request.form["last_name"], request.form["password"])
+    print(response)
+    if response != "OK":
+        return render_template("pages/signup.html", error="Error")
     else:
         return redirect(url_for("login"), code=302)
 
 @app.route("/login")
 def loginPage():
-    return render_template("pages/login")
+    return render_template("pages/login.html")
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -29,7 +37,11 @@ def login():
         session["user"] = request.form['email']
         return redirect(url_for("dashboard"))
 
-@app.route("dashboard")
+@app.route("/dashboard")
 def dashboard():
     logs = reqs.get_logs(session["user"], 0)
-    return render_template("pages/logs", logs=logs)
+    return render_template("pages/dashboard.html", logs=logs)
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=3000, debug=True)
+
--- reqs.py
+++ reqs.py
@@ -1,15 +1,15 @@
 import requests
 
-BACKEND_URL="localhost:3000"
+BACKEND_URL="http://localhost:8080"
 
 def signup(email, first, last, password):
-    params = {"email" : email, "first" : first, "last" : last, "password" : password}
-    return requests.post(BACKEND_URL + "/signup", params)
+    params = {"email" : email, "first_name" : first, "last_name" : last, "password" : password}
+    return requests.post(BACKEND_URL + "/signup", json=params)
 
 def login(email, password):
     params = {"email" : email, "password" : password}
-    return requests.post(BACKEND_URL + "login", params)
+    return requests.post(BACKEND_URL + "login", json=params)
 
 def get_logs(user, pagination, searchParams=[]):
     params = {"user" : user, "pagination" : pagination, "searchParams" : searchParams}
-    return requests.post(BACKEND_URL + "/logs", params)
+    return requests.post(BACKEND_URL + "/logs", json=params)
