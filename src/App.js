f34e4d7b322f4f9f69e8a0e9c1b88f564c2b3a07
NeilJain56 <neiljain56@gmail.com>
2020-10-18 14:02:36 -0400

    New Routes and added fields

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./Login/Login.js";
+import Signup from "./Signup/Signup.js";
 
 function App() {
   return (
     <Router>
-      <Route path="/login" component={Login} />
+      <Route path="/Signup" component={Signup} />
     </Router>
   );
 }
--- src/Signup/SignupCard/Signup.js
+++ src/Signup/SignupCard/Signup.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
-import "./LoginCard.css";
+import "./SignupCard.css";
 import { Form, Button, Container, Row, Col, Collapse } from "react-bootstrap";
 
-export default class LoginCard extends Component {
+export default class SignupCard extends Component {
   constructor(props) {
     super(props);
   }
 
-  handleLogin(event) {
+  handleSignup(event) {
     event.preventDefault();
   }
 
   render() {
     return (
-      <Container className="login-container">
+      <Container className="signup-container">
         <Row>
           <Col
             sm={{ span: 12 }}
@@ -23,38 +23,38 @@ export default class LoginCard extends Component {
             <div>
               <p className="welcome-text">Welcome!</p>
             </div>
-            <div className="d-flex flex-column login-card p-3 p-lg-5">
+            <div className="d-flex flex-column signup-card p-3 p-lg-5">
               <div>
-                <Form onSubmit={(e) => this.handleLogin(e)}>
+                <Form onSubmit={(e) => this.handleSignup(e)}>
                 <Form.Group controlId="firstName">
                     <Form.Control
                       type="text"
-                      className="login-input"
+                      className="signup-input"
                       placeholder="First Name"
                     />
                   </Form.Group>
                   <Form.Group controlId="lastName">
                     <Form.Control
                       type="text"
-                      className="login-input"
+                      className="signup-input"
                       placeholder="Last Name"
                     />
                   </Form.Group>
                   <Form.Group controlId="formBasicEmail">
                     <Form.Control
                       type="email"
-                      className="login-input"
+                      className="signup-input"
                       placeholder="Email Address"
                     />
                   </Form.Group>
                   <Form.Group controlId="formBasicPassword">
                     <Form.Control
                       type="password"
-                      className="login-input"
+                      className="signup-input"
                       placeholder="Password"
                     />
                   </Form.Group>
-                  <Button block size="lg" type="submit" className="login-btn">
+                  <Button block size="lg" type="submit" className="signup-btn">
                     <span>Sign Up</span>
                   </Button>
                 </Form>
